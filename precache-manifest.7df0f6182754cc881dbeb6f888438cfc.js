bfd5f0d6538b200c09325f04151bf372f0bd48cf
nywooz <nywooz7@gmail.com>
2019-09-09 19:54:41 +0100

    Updates

---
+++ precache-manifest.7df0f6182754cc881dbeb6f888438cfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b2675f07b722cea94660ff0734e3a84",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "664414b8d562afc51810",
+    "url": "/weather-app/static/css/2.1e66eaca.chunk.css"
+  },
+  {
+    "revision": "d5af8054097eb80fafdd",
+    "url": "/weather-app/static/css/main.d09f02c4.chunk.css"
+  },
+  {
+    "revision": "664414b8d562afc51810",
+    "url": "/weather-app/static/js/2.5cb857e3.chunk.js"
+  },
+  {
+    "revision": "d5af8054097eb80fafdd",
+    "url": "/weather-app/static/js/main.99cec636.chunk.js"
+  },
+  {
+    "revision": "c4b5af54619c263c6892",
+    "url": "/weather-app/static/js/runtime~main.5cfb4b6d.js"
+  }
+]);
