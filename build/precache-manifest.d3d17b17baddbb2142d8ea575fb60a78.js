69f63c7306ea9244658e39b34a76f082fac67968
nywooz <nywooz7@gmail.com>
2019-09-09 19:25:00 +0100

    fix build and deploy script

---
+++ build/precache-manifest.d3d17b17baddbb2142d8ea575fb60a78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1415a445f16983251658a4308e85e82a",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "664414b8d562afc51810",
+    "url": "/weatherapp/static/css/2.1e66eaca.chunk.css"
+  },
+  {
+    "revision": "bfa319d0c06c9b0b4c30",
+    "url": "/weatherapp/static/css/main.d09f02c4.chunk.css"
+  },
+  {
+    "revision": "664414b8d562afc51810",
+    "url": "/weatherapp/static/js/2.5cb857e3.chunk.js"
+  },
+  {
+    "revision": "bfa319d0c06c9b0b4c30",
+    "url": "/weatherapp/static/js/main.548291ce.chunk.js"
+  },
+  {
+    "revision": "4486215fc94af9867c1e",
+    "url": "/weatherapp/static/js/runtime~main.96f99bc3.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.807ac29267d279b0afb570e76d57030f.js"
+  "/weatherapp/precache-manifest.d3d17b17baddbb2142d8ea575fb60a78.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/weatherapp/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
