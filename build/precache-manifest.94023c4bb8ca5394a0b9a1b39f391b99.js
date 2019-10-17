48a5f209b6db8b5ef19ad9ca1a6301d6680b7812
nywooz <nywooz7@gmail.com>
2019-10-17 01:19:27 +0100

    update read me, gh pages

---
+++ build/precache-manifest.94023c4bb8ca5394a0b9a1b39f391b99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a9b796795e85ab6aafef4fe4558c8c8",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "96f39079409f73b8dfbc",
+    "url": "/weather-app/static/css/2.1e66eaca.chunk.css"
+  },
+  {
+    "revision": "8b08ef850179b526c53d",
+    "url": "/weather-app/static/css/main.e4f1cf3d.chunk.css"
+  },
+  {
+    "revision": "96f39079409f73b8dfbc",
+    "url": "/weather-app/static/js/2.bd049794.chunk.js"
+  },
+  {
+    "revision": "8b08ef850179b526c53d",
+    "url": "/weather-app/static/js/main.628a6473.chunk.js"
+  },
+  {
+    "revision": "ae29bb779b43c1743bc5",
+    "url": "/weather-app/static/js/runtime~main.b214cd7e.js"
+  }
+]);
