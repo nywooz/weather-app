98f6dc1e4b9a3b63c9c8484e6f0670bceffb315b
nywooz <nywooz7@gmail.com>
2019-10-17 01:18:23 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/weather-app/precache-manifest.cce880ac6ab02b227fe4828496df1c73.js"
+  "/weather-app/precache-manifest.94023c4bb8ca5394a0b9a1b39f391b99.js"
 );
 
 self.addEventListener('message', (event) => {
